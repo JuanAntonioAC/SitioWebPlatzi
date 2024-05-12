adbb288cccb4768f4ec8ee69aaf9760940c75479
JuanAntonioAC <ingjuanantonioaguirrec@gmail.com>
2024-05-12 14:58:00 -0600

    imports

--- obj/Debug/netcoreapp2.1/PlatziWeb.AssemblyInfo.cs
+++ obj/Debug/netcoreapp2.1/PlatziWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatziWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa4aa7f0df841d3829a1675566150edcc2d2e677")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+278f0c9d06f79ef9c00c60f239d94b392d1bb717")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatziWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatziWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
