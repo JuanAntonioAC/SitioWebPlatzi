a4ff26d6a2f251a2d79acd8aa7ffe0c168a52a43
JuanAntonioAC <ingjuanantonioaguirrec@gmail.com>
2024-05-11 19:05:21 -0600

    proyecto cambio carpeta

--- PlatziWeb/obj/Debug/netcoreapp2.1/PlatziWeb.AssemblyInfo.cs
+++ PlatziWeb/obj/Debug/netcoreapp2.1/PlatziWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatziWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6875f9eb0c888312be70fcd29e6a63b02c2e34e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatziWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatziWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
